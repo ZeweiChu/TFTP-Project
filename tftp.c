aecf00aa3610c933cf71125dce30ce28cf375a95
jasonchuzewei@gmail.com <jasonchuzewei@gmail.com>
2013-05-15 09:22:41 -0700

    changes to tftp.c

---
+++ tftp.c
@@ -0,0 +1,57 @@
+#include "tftp.h"
+
+recvData(int sockfd, struct sockaddr *pserv_addr, int servlen, char *filename){
+	int n, clilen;
+	
+	short signed block = 0;
+	
+	struct TFTP_DATA *dataPacket = malloc(sizeof(struct TFTP_DATA));
+	struct TFTP_ACK *ack = malloc(sizeof(struct TFTP_ACK));
+	FILE *fp = fopen(filename, "w");
+	char buffer[MAX];
+
+	for (;;){
+		clilen = sizeof(struct sockaddr);
+
+		n = recvfrom(sockfd, dataPacket, sizeof(struct TFTP_DATA), 0, pserv_addr, &servlen);
+		if (n < 0) {
+			printf(stderr, "Error on receiving DATA\n");
+			//TODO: 
+		}
+
+		if (ntohs(dataPacket->opcode) == ERROR) {
+			printf(stderr, "Error received - %s\n", errMsg[ntohs(dataPacket->block)]);
+			//TODO: 
+		}
+
+		int length = strlen(dataPacket->data);
+		(buffer, dataPacket->data);
+		
+	}
+}
+
+sendData(int sockfd, struct sockaddr *recv_addr, int recvlen, char *filename){
+	int n, len;
+
+	signed short int block = 0;
+	FILE *fp = fopen(filename, "r");
+	char buffer[MAX];
+
+	if (fp == NULL){
+		//TODO: print out error mesg and send error mesg
+	}
+
+	struct TFTP_DATA * dataPacket;
+	struct TFTP_ACK * ack;
+	dataPacket = (struct TFTP_DATA *)malloc(sizeof(struct TFTP_DATA));
+	memset(dataPacket, 0, sizeof(struct TFTP_DATA));
+	ack = (struct TFTP_ACK*)malloc(sizeof(struct TFTP_ACK));
+	memset(ack, 0, sizeof(struct TFTP_ACK));
+
+
+}
+
+processError()
+
+
+int main()
--- tftp.h
+++ tftp.h
@@ -2,11 +2,37 @@
 
 #include <>
 
+#define MAX 				512
+#define HALF_MAX 			255
+#define MAX_TIMEOUTS		10
 
-#define RRQ 		1
-#define WRQ			2
-#define DATA 		3
-#define ACK  		4
-#define ERROR 		5	
+
+#define RRQ 				1
+#define WRQ					2
+#define DATA 				3
+#define ACK  				4
+#define ERROR 				5	
+
+
+
+
+struct TFTP_RQ{
+	signed short int opcode;
+	char filename[HALF_MAX];
+	char zero_0;
+	char mode[HALF_MAX];
+	char zero_1;
+};
+
+struct TFTP_DATA{
+	signed short int opcode;
+	signed short int block;
+	char data[MAX];
+};
+
+struct TFTP_ACK{
+	signed short int opcode;
+	signed short int block;
+};
 
 
